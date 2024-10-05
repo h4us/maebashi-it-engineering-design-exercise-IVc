baa9969ae97425078820a3a8ba644bbdb0754815
Takanobu INAFUKU <takanobu.inafact@gmail.com>
2024-10-05 14:26:02 +0900

    Samples 1 - 3

---
+++ 20241008/1008_1/1008_1.ino
@@ -0,0 +1,23 @@
+// Arduinoの出力ピンにスケッチ内で使う別名を付ける
+// 7番ピンを _SW1 という名前に、8番ピンを _LED1 という名前に
+#define _SW1 7
+#define _LED1 8
+
+void setup() {
+  // _SW1（7番ピン）を入力にする
+  pinMode(_SW1, INPUT); 
+
+  // _SW1（7番ピン）を入力にする・「プルアップ」バージョン
+  // pinMode(_SW1, INPUT_PULLUP);  
+
+  // _LED1（8番ピン）を出力にする
+  pinMode(_LED1, OUTPUT);
+}
+
+void loop() { 
+  // デジタル入力から読み取ったタクトスイッチの状態を使って、LEDの点灯状態を制御する
+  digitalWrite(_LED1, digitalRead(_SW1)); 
+  
+  // 「!」という記号（論理否定演算子）を使って、押す・離すの挙動を反転させたバージョン
+  // digitalWrite(_LED1, !digitalRead(_SW1)); 
+}
---
+++ 20241008/1008_2/1008_2.ino
@@ -0,0 +1,35 @@
+// Arduinoの出力ピンにスケッチ内で使う別名を付ける
+// 7番ピンを _SW1 という名前に、8番ピンを _LED1 という名前に
+#define _SW1 7
+#define _LED1 8
+
+// デジタル入力読み取り用の変数（タクトスイッチの状態を記憶する領域）を設定する
+int swState = 0;
+
+void setup() {
+  // _SW1（7番ピン）を入力にする・「プルアップ」バージョン
+  pinMode(_SW1, INPUT_PULLUP);  
+
+  // _LED1（8番ピン）を出力にする
+  pinMode(_LED1, OUTPUT);
+
+  // シリアルモニタ用の通信の初期化処理
+  Serial.begin(19200);
+}
+
+void loop() {
+  // デジタル入力から読み取ったタクトスイッチの状態を変数に記憶する
+  swState = !digitalRead(_SW1); 
+
+  // シリアルモニタに表示するメッセージ（改行なし）
+  Serial.print("_SW1 State: ");
+   // シリアルモニタに表示するメッセージ（改行あり） 
+  Serial.println(swState);
+
+  // 変数を使ってLEDの点灯状態を変える
+  digitalWrite(_LED1, swState); 
+
+  // 200ミリ秒待つ
+  // （待ち時間を入れないと、タクトスイッチを押す・離すの動作による変化が早すぎてわかりにくいので）
+  delay(200); 
+}
