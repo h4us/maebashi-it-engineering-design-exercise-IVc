353bf6a8de7741b542664283dff0ca316eb47fa0
Takanobu INAFUKU <takanobu.inafact@gmail.com>
2024-11-05 11:25:26 +0900

    20241105 - 1

---
+++ _src/20241105/1105_servo_simple/1105_servo_simple.ino
@@ -0,0 +1,37 @@
+/* 1105_servo_simple.ino */
+
+#include <Servo.h>
+
+#if (defined(PICO_RP2040))
+#define _SRV_OUTPIN 0
+#else
+#define _SRV_OUTPIN 9
+#endif
+
+Servo srv1;
+
+int angle = 0;
+
+void setup() {
+  pinMode(_SRV_OUTPIN, OUTPUT);
+
+  // サーボモーター（SG90）の初期化。
+  // 指定可能なパルス幅の範囲が異なる場合は初期化時に指定する。
+  srv1.attach(_SRV_OUTPIN, 500, 2400);
+  srv1.write(angle);
+
+  Serial.begin(57600);
+}
+
+void loop() {
+  srv1.write(angle);
+
+  Serial.print("Angle: ");
+  Serial.println(angle);
+
+  // 2秒間隔で30°ずつ動かす。180°まで行ったら0°に戻す.
+  angle += 30;
+  if (angle > 180) angle = 0;
+
+  delay(2000);
+}
---
+++ _src/20241105/1105_servo_with_serial/1105_servo_with_serial.ino
@@ -0,0 +1,106 @@
+/* 1105_servo_with_serial */
+
+#include <Servo.h>
+
+#if (defined(PICO_RP2040))
+#define _SRV_OUTPIN 0
+#else
+#define _SRV_OUTPIN 9
+#endif
+
+Servo srv1;
+
+int angle = 90;
+
+unsigned long tick = 0;
+
+const byte numChars = 64;
+char receivedChars[numChars];
+
+bool newData = false;
+
+void setup() {
+  pinMode(_SRV_OUTPIN, OUTPUT);
+
+  // サーボモーター（SG90）の初期化。
+  // 指定可能なパルス幅の範囲が異なる場合は初期化時に指定する。
+  srv1.attach(_SRV_OUTPIN, 500, 2400);
+  srv1.write(angle);
+
+  Serial.begin(57600);
+}
+
+void loop() {
+  recvWithEndMarker();
+
+  if (newData == true) {
+    String tmp_str = String(receivedChars);
+    String tmp_perms[2] = { "", "" };
+    int sidx = 0, pidx = 0;
+
+    while (true) {
+      int fidx = tmp_str.indexOf(' ', sidx);
+      if (fidx != -1) {
+        String sstr = tmp_str.substring(sidx, fidx);
+        sidx = fidx + 1;
+        if (pidx < 2) {
+          tmp_perms[pidx] = sstr;
+          pidx ++;
+        } else {
+          break;
+        }
+      } else {
+        String rstr = tmp_str.substring(sidx, tmp_str.length());
+        if (pidx < 2) {
+          tmp_perms[pidx] = rstr;
+        }
+        break;
+      }
+    }
+
+    Serial.println(tmp_str);
+
+    if (tmp_perms[0].length() != 0) {
+      angle = map(tmp_perms[0].toInt(), 0, 1023, 0, 180);
+
+      Serial.print("Raw[0]:");
+      Serial.print(tmp_perms[0]);
+      Serial.print(",Angle:");
+      Serial.println(angle);
+
+      srv1.write(angle);
+    }
+
+    newData = false;
+  }
+
+  if ((millis() - tick) > 1000) {
+    tick = millis();
+    Serial.println("tick");
+  }
+}
+
+void recvWithEndMarker() {
+  static byte ndx = 0;
+  const char endMarker = ';';
+  char rc;
+
+  if (Serial.available()) {
+    while (Serial.available() > 0 && newData == false) {
+      rc = Serial.read();
+
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+          ndx = numChars - 1;
+        }
+      }
+      else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        ndx = 0;
+        newData = true;
+      }
+    }
+  }
+}
